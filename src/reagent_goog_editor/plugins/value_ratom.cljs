468913901bf0f523ce024ed51e3ea74aeea6699b
Danny Guinther <danny@zensight.co>
2017-06-13 13:53:44 -0400

    Deref value ratom on change in case it's a lazy cursor

--- src/reagent_goog_editor/plugins/value_ratom.cljs
+++ src/reagent_goog_editor/plugins/value_ratom.cljs
@@ -9,7 +9,13 @@
 (defn on-change [editor ratom evnt]
   (let [contents (editor/get-field-contents (.-target evnt))]
     (when-not (= contents @ratom)
-      (reset! ratom contents))))
+      (reset! ratom contents)
+      ;; Deref the atom again in case it is a reagent cursor. This forces the
+      ;; underlying value to be updated in the case of a cursor and shouldn't
+      ;; incur any additional cost in the case of an atom. This remedies the
+      ;; situation where changes come in so quickly from the editor that a
+      ;; cursor will get out of sync if we allow it to lazily update.
+      @ratom)))
 
 (defmethod plugin-api/attach plugin [_ editor ratom]
   (-> (fn [] (editor/set-html editor @ratom))
