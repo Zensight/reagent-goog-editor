723819534b4e06ff4532c12fb51016287a284a34
Danny Guinther <danny@zensight.co>
2017-06-13 14:03:06 -0400

    Version 1.0.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject reagent-goog-editor "1.0.1"
+(defproject reagent-goog-editor "1.0.2-SNAPSHOT"
   :description "Reagent wrapper around goog.editor"
   :url "https://github.com/Zensight/reagent-goog-editor"
   :license {:name "Copyright 2017 Zensight"
